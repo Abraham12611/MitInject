6e2a428a7ca9fa951cb329364fb555dd97ad2879
Abraham12611 <abraham.dahunsi@gmail.com>
2025-03-13 13:11:29 +0000

    new-fix

--- next.config.js
+++ next.config.js
@@ -1,59 +1,59 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    loader: 'default',
-    path: '',
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'unavatar.io',
-        pathname: '/twitter/**',
-      },
+    images: {
+      unoptimized: true,
+      loader: 'default',
+      path: '',
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'unavatar.io',
+          pathname: '/twitter/**',
+        },
+      ],
+    },
+    transpilePackages: [
+      '@privy-io/react-auth',
+      '@privy-io/wagmi-connector',
+      'magic-sdk',
+      '@magic-sdk/provider',
+      '@magic-sdk/commons',
+      '@magic-sdk/types'
     ],
-  },
-  transpilePackages: [
-    '@privy-io/react-auth',
-    '@privy-io/wagmi-connector',
-    'magic-sdk',
-    '@magic-sdk/provider',
-    '@magic-sdk/commons',
-    '@magic-sdk/types'
-  ],
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      buffer: require.resolve('buffer'),
-    };
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'magic-sdk': require.resolve('magic-sdk'),
-      '@magic-sdk/provider': require.resolve('@magic-sdk/provider'),
-      '@magic-sdk/commons': require.resolve('@magic-sdk/commons'),
-      '@magic-sdk/types': require.resolve('@magic-sdk/types'),
-    };
-    return config;
-  },
-  experimental: {
-    turbo: {
-      rules: {
-        '*.js': ['babel-loader'],
-        '*.jsx': ['babel-loader'],
-        '*.ts': ['ts-loader'],
-        '*.tsx': ['ts-loader']
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer'),
+      };
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'magic-sdk': require.resolve('magic-sdk'),
+        '@magic-sdk/provider': require.resolve('@magic-sdk/provider'),
+        '@magic-sdk/commons': require.resolve('@magic-sdk/commons'),
+        '@magic-sdk/types': require.resolve('@magic-sdk/types'),
+      };
+      return config;
+    },
+    experimental: {
+      turbo: {
+        rules: {
+          '*.js': ['babel-loader'],
+          '*.jsx': ['babel-loader'],
+          '*.ts': ['ts-loader'],
+          '*.tsx': ['ts-loader']
+        }
       }
-    }
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
-
-module.exports = nextConfig
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+  }
+  
+  module.exports = nextConfig
