f7b534d900c4990eb66d65fa016d9861c25395d5
Abraham12611 <abraham.dahunsi@gmail.com>
2025-03-14 13:33:09 +0000

    update injective

--- src/utils/injective-client.ts
+++ src/utils/injective-client.ts
@@ -1,8 +1,8 @@
 import {
   MsgBroadcaster,
-  WalletStrategy
 } from '@injectivelabs/wallet-core'
-import{ Network } from '@injectivelabs/networks'
+import { WalletStrategy } from '@injectivelabs/wallet-strategy'
+import { Network } from '@injectivelabs/networks'
 import { EthereumChainId, ChainId } from '@injectivelabs/ts-types'
 import { 
   IndexerGrpcSpotApi, 
